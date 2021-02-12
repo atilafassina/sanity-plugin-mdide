8f1488a477bed3b1d8b71c339e769a24ccc9a50a
Atila Fassina <atila@fassina.eu>
2021-02-12 18:26:11 +0100

    migrate to css modules - fix ui issues

--- src/md-editor.jsx
+++ src/md-editor.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event'
-import SimpleMDE from 'react-simplemde-editor'
-import './styles.css?raw'
+import React, { Component } from 'react';
+import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
+import SimpleMDE from 'react-simplemde-editor';
+import styles from './base-styles.css';
+// import './styles.css?raw';
 
 const createPatchFrom = (value) =>
-  PatchEvent.from(value === '' ? unset() : set(value))
+  PatchEvent.from(value === '' ? unset() : set(value));
 
 class MdEditor extends Component {
   render() {
     const {
       minHeight = '500px',
       hasLineNumbers = false,
-    } = this.props.type.options
-
+    } = this.props.type.options;
     return (
       <SimpleMDE
         value={this.props.value}
@@ -22,8 +22,8 @@ class MdEditor extends Component {
           minHeight: minHeight,
         }}
       />
-    )
+    );
   }
 }
 
-export default MdEditor
+export default MdEditor;
