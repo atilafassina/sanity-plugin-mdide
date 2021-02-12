ee137c1f85a526d15e81b82a9b536f18a88e93f1
Atila Fassina <atila@fassina.eu>
2021-02-12 17:29:06 +0100

    fix schema path in sanity.json

--- schema-type.js
+++ schema-type.js
@@ -1,8 +1,8 @@
-import MdEditor from './editor'
+import MdEditor from './dist/md-editor';
 
 export default {
   name: 'markdown',
   title: 'MarkdownIDE',
   type: 'string',
   inputComponent: MdEditor,
-}
+};
