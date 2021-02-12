bb3e1d7754b7a34c1b578edb8b4ac7f03faf6c73
Atila Fassina <atila@fassina.eu>
2021-02-12 19:34:14 +0100

    improve styles for both dark and light mode

--- src/md-editor.jsx
+++ src/md-editor.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 import SimpleMDE from 'react-simplemde-editor';
-import styles from './base-styles.css';
-// import './styles.css?raw';
+import './base-styles.css';
 
 const createPatchFrom = (value) =>
   PatchEvent.from(value === '' ? unset() : set(value));
 
 class MdEditor extends Component {
   render() {
-    const {
-      minHeight = '500px',
-      hasLineNumbers = false,
-    } = this.props.type.options;
+    const minHeight = this.props.type
+      ? this.props.type.options
+        ? this.props.type.minHeight
+        : undefined
+      : '500px';
     return (
       <SimpleMDE
         value={this.props.value}
         onChange={(value) => this.props.onChange(createPatchFrom(value))}
         options={{
-          lineNumbers: hasLineNumbers,
-          minHeight: minHeight,
+          lineNumbers: false,
+          minHeight,
         }}
       />
     );
